1b650807ce9e502c650c9f7a0a7418aabd61896b
Ozan <ozan.yasin97@gmail.com>
2022-10-21 13:51:30 +0300

    JS-Projects - Movie-Fight UPDATED

--- Movie-Fight/index.js
+++ Movie-Fight/index.js
@@ -2,13 +2,29 @@
 // http://www.omdbapi.com/?apikey=[yourkey]&
 // API Key: ccdeef6c
 
-const fetchData = async () => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+const fetchData = async (searchTerm) => {
+  const response = await axios.get('http://www.omdbapi.com', {
     params: {
       apikey: 'ccdeef6c',
-      s: 'avengers',
+      s: searchTerm,
     },
   });
 
   console.log(response.data);
 };
+
+const input = document.querySelector('input');
+
+let timeoutId;
+const onInput = (event) => {
+  if (timeoutId) {
+    clearTimeout(timeoutId);
+  }
+  // second time they press the key to input, we're going to once again enter onInput function;
+  // Then, timeoutId will be defined. Which means, that's executes clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    fetchData(event.target.value);
+  }, 500);
+};
+
+input.addEventListener('input', onInput);
