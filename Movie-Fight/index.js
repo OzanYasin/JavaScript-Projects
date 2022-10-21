47d5cfab15b2a27947edef578cfeffb6be26eb06
Ozan <ozan.yasin97@gmail.com>
2022-10-21 14:08:56 +0300

    JS-Projects - Movie-Fight debounce function added

--- Movie-Fight/index.js
+++ Movie-Fight/index.js
@@ -15,16 +15,23 @@ const fetchData = async (searchTerm) => {
 
 const input = document.querySelector('input');
 
-let timeoutId;
-const onInput = (event) => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  // second time they press the key to input, we're going to once again enter onInput function;
-  // Then, timeoutId will be defined. Which means, that's executes clearTimeout(timeoutId);
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+    // second time they press the key to input, we're going to once again enter onInput function;
+    // Then, timeoutId will be defined. Which means, that's executes clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      // apply -> call the function as we normally would and take all the arguments or whatever is inside of that array, and pass them in as separate arguments to the original function
+      func.apply(null, args);
+    }, delay);
+  };
 };
 
-input.addEventListener('input', onInput);
+const onInput = (event) => {
+  fetchData(event.target.value);
+};
+
+input.addEventListener('input', debounce(onInput, 500));
