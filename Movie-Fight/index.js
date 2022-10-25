32d310a31fad88b995cd1be249eaeae917fa84d7
ozan <ozan.yasin97@gmail.com>
2022-10-25 20:02:35 +0300

    JS-Projects - movie-fight app UPDATED

--- Movie-Fight/index.js
+++ Movie-Fight/index.js
@@ -55,6 +55,7 @@ const onInput = async (event) => {
     option.addEventListener('click', (event) => {
       dropdown.classList.remove('is-active');
       input.value = movie.Title;
+      onMovieSelect(movie);
     });
   }
 };
@@ -67,3 +68,33 @@ document.addEventListener('click', (event) => {
     dropdown.classList.remove('is-active');
   }
 });
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: 'ccdeef6c',
+      i: movie.imdbID,
+    },
+  });
+
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+      <p class="image">
+        <img src="${movieDetail.Poster}" alt="${movieDetail.Title}" />
+      </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+        <h1>${movieDetail.Title}</h1>
+        <h4>${movieDetail.Genre}</h4>
+        <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+`;
+};
