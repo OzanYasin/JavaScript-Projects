c405e15159fa9cb7a32e91ba2e98710f48216556
ozan <ozan.yasin97@gmail.com>
2022-10-26 19:38:29 +0300

    movie-fight refactoring START

--- Movie-Fight/index.js
+++ Movie-Fight/index.js
@@ -17,56 +17,14 @@ const fetchData = async (searchTerm) => {
   return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search for a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async (event) => {
-  const movies = await fetchData(event.target.value);
-  // console.log(movies);
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  dropdown.classList.add('is-active');
-  resultsWrapper.innerHTML = '';
-  for (let movie of movies) {
-    const option = document.createElement('a');
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
-    <img src="${imgSrc}" />
-    ${movie.Title}
-    `;
-    resultsWrapper.appendChild(option);
-    option.addEventListener('click', (event) => {
-      dropdown.classList.remove('is-active');
-      input.value = movie.Title;
-      onMovieSelect(movie);
-    });
-  }
-};
-
-input.addEventListener('input', debounce(onInput, 500));
-
-// Events bubble, which means if some element contained inside the document, and if it does not handled, then the event is going to essentially bubble up until it gets to the top level of our entire HTML document. In this case, its document
-document.addEventListener('click', (event) => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
+createAutoComplete({
+  root: document.querySelector('.autocomplete'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-two'),
+});
+createAutoComplete({
+  root: document.querySelector('.autocomplete-three'),
 });
 
 const onMovieSelect = async (movie) => {
