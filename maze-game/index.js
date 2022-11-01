c626e06b42ea4a93da74b5fb4bf8ef4f48f42ae3
ozan <ozan.yasin97@gmail.com>
2022-11-01 19:08:55 +0300

    maze-game app UPDATED

--- maze-game/index.js
+++ maze-game/index.js
@@ -3,5 +3,35 @@
 // * How are we going to draw this thing on the screen? - matter-js
 // * How do we make some keyboard keys control the ball? - matter-js
 // * How do we detect when the ball touches the green square? - matter-js
+// https://brm.io/matter-js/
 
-console.log('hi there!');
+// Matter JS Terminology
+
+// * World *    Object that contains all of the different 'things' in our matter app
+// * Engine *   Reads in the current state of the world from the world object, then calculates changes in position of all the different shapes
+// * Runner *   Gets the engine and world to work together. Runs about 60 times per second.
+// * Render *   Whenever the engine process an update, Render will take a look at all the different shapes and show them on the screen.
+// * Body *     A shape that we are displaying. Can be a circle, rectangle, oval, etc.
+
+const { World, Engine, Runner, Render, Bodies } = Matter;
+
+const engine = Engine.create();
+// So, technically when we create an engine, we get a world object along with it.
+const { world } = engine;
+const render = Render.create({
+  element: document.body,
+  engine: engine,
+  options: {
+    width: 800,
+    height: 600,
+  },
+});
+Render.run(render);
+// The runner is what coordinates all these changes from state A to state B of our engine.
+Runner.run(Runner.create(), engine);
+
+const shape = Bodies.rectangle(200, 200, 50, 50, {
+  isStatic: true,
+});
+
+World.add(world, shape);
