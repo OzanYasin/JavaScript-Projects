a334f9eb99b56c775f1540c1218b16e695ba3be6
ozan <ozan.yasin97@gmail.com>
2022-11-01 18:36:16 +0300

    NEW PROJECT! / maze-game app START

---
+++ maze-game/index.js
@@ -0,0 +1,7 @@
+// Big Challenges
+// * How do we generate a maze? - tree data structure + recursion to implement the simplest algorithm.
+// * How are we going to draw this thing on the screen? - matter-js
+// * How do we make some keyboard keys control the ball? - matter-js
+// * How do we detect when the ball touches the green square? - matter-js
+
+console.log('hi there!');
