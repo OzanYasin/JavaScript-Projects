47b16cd93c11a5ef16558095e1573d21b521b523
ozan <ozan.yasin97@gmail.com>
2022-11-02 15:02:13 +0300

    maze-game app UPDATED (drag and drop physic functionality added)

--- maze-game/index.js
+++ maze-game/index.js
@@ -13,7 +13,11 @@
 // * Render *   Whenever the engine process an update, Render will take a look at all the different shapes and show them on the screen.
 // * Body *     A shape that we are displaying. Can be a circle, rectangle, oval, etc.
 
-const { World, Engine, Runner, Render, Bodies } = Matter;
+const { World, Engine, Runner, Render, Bodies, MouseConstraint, Mouse } =
+  Matter;
+
+const width = 800;
+const height = 600;
 
 const engine = Engine.create();
 // So, technically when we create an engine, we get a world object along with it.
@@ -22,13 +26,20 @@ const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600,
+    width,
+    height,
   },
 });
 Render.run(render);
 // The runner is what coordinates all these changes from state A to state B of our engine.
 Runner.run(Runner.create(), engine);
+
+World.add(
+  world,
+  MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas),
+  })
+);
 
 // Walls
 const walls = [
@@ -39,5 +50,7 @@ const walls = [
 ];
 
 World.add(world, walls);
+
+// Random Shapes
 
 World.add(world, Bodies.rectangle(200, 200, 50, 50));
