105d0dcb5ace66eb6e83e700e9d33d69893fa5ae
ozan <ozan.yasin97@gmail.com>
2022-11-04 22:01:00 +0300

    maze-game app UPDATED

--- maze-game/index.js
+++ maze-game/index.js
@@ -74,9 +74,37 @@ const horizontals = Array(cells - 1)
   .fill(null)
   .map(() => Array(cells).fill(false));
 
-console.log(grid);
-console.log(verticals);
-console.log(horizontals);
-
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
+
+const recurse = (row, column) => {
+  // If I have visited the cell at [row, column] then return
+  if (grid[row][column] === true) {
+    return;
+  }
+
+  // Mark this cell as being visited
+  grid[row][column] = true;
+
+  // Assemble randomly-ordered list of neighbors
+  const neighbors = [
+    [row - 1, column],
+    [row + 1, column],
+    [row, column - 1],
+    [row, column + 1],
+  ];
+
+  // For each neighbor...
+
+  // Check if that neighbor is out of bounds
+
+  // If we have visited that neighbor, continue to next neighbor
+
+  // Remove a wall from either horizontals or verticals array
+
+  // Visit that next cell
+};
+
+recurse(startRow, startColumn);
+
+console.log(grid);
