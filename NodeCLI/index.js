bb6ce863033efba04d31076d5d7605fe205f6f7d
ozan <ozan.yasin97@gmail.com>
2022-11-10 17:03:15 +0300

    NodeCLI app  UPDATED

--- NodeCLI/index.js
+++ NodeCLI/index.js
@@ -1,1 +1,17 @@
-console.log('welcome to NodeJS Command Line Tools');
+const message = require('./myScript');
+
+console.log(message);
+
+// !! Node Arguments !!
+
+// exports - Equivalent to 'module.exports'. We can technically export code using this, but it is easier to use 'module.exports' because of a little corner case.
+
+// require - Function to get access to the exports from another file
+
+// module - Object that defines some properties + information about the current fil
+
+// __filename - Full path + file name of this file
+
+// __dirname - Full path of this file
+
+console.log(arguments);
