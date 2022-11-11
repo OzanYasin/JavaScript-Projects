05f05b8f20077877ff26c11c1e065c5d695ae9df
ozan <ozan.yasin97@gmail.com>
2022-11-11 19:52:37 +0300

    node-cli UPDATED

--- node-cli/index.js
+++ node-cli/index.js
@@ -1,7 +1,3 @@
-const message = require('./myScript');
-
-// console.log(message);
-
 // !! Node Arguments !!
 
 // exports - Equivalent to 'module.exports'. We can technically export code using this, but it is easier to use 'module.exports' because of a little corner case.
@@ -15,4 +11,34 @@ const message = require('./myScript');
 // __dirname - Full path of this file
 
 // console.log(arguments);
-console.log(require.cache);
+// console.log(require.cache);
+
+const counterObject = require('./myScript');
+
+console.log(counterObject.getCounter()); // 0
+counterObject.incrementCounter();
+console.log(counterObject.getCounter()); // 0 1
+
+const newCounterObject = require('./myScript');
+
+// It's going to invoke getCounter function from the require cache, not from the myScript.js as first time it requires.
+console.log(newCounterObject.getCounter()); // 0 1 1
+
+//  !! Debugging NodeJS !!
+
+// > node inspect index.js
+// Start up a debugger CLI and pause execution whenever a 'debugger' statement is hit.
+
+// > node --inspect index.js
+// Start up a debugger instance and pause execution whenever a 'debugger' statement is hit. Access the debugger at 'chrome://inspect'.
+
+// > node --inspect-brk index.js
+// Start up a debugger instance and wait to execute until a debugger is connected. Access the debugger at 'chrome://inspect'.
+
+// * CLI Debugger Commands
+
+// c -> Continue execution until program ends or next debugger statement
+// n -> Run the next line of code
+// s -> Step in to a function
+// o -> Step out of the current function
+// repl -> Start up an execution environment where we can reference the different variables inside of our program.
