88cad6f79ac2591f3101cf1872c68ce49a4094a4
Ozan Yasin <ozn@Ozans-MacBook-Air.local>
2022-11-17 20:37:42 +0300

    project-runner START (new project!)

---
+++ project-runner/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+// ! First things to do:
+// ! Writing <npm link> on this project's directory terminal
+
+// If you are on macOS or linux based operator system you need to run the code below:
+// chmod +x index.js
+// chmod +x on a file (your script) only means, that you'll make it executable.
+
+// -----------------------------
+
+// !! Big Issues !!
+
+// 1) Need to detect if a file changes,
+// 2) It would be nice to provide some help to users of our CLI tool,
+// 3) Need to figure out how to execute some JS code from within a JS program.
+
+console.log('hello');
