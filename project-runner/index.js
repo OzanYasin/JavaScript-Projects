7cb335a96ce524b5a40500b0878831cbaeca6b7d
OzanYasin <ozan.yasin97@gmail.com>
2022-11-18 18:49:11 +0300

    project-runner node app / bug fixed

--- project-runner/index.js
+++ project-runner/index.js
@@ -46,9 +46,13 @@ program
       throw new Error(`Could not find the file ${name}`);
     }
 
+    let proc;
     const start = debounce(() => {
+      if (proc) {
+        proc.kill();
+      }
       // The child_process.spawn() method spawns a new process using the given command, with command-line arguments in args. If omitted, args defaults to an empty array.
-      spawn('node', [name], { stdio: 'inherit' });
+      proc = spawn('node', [name], { stdio: 'inherit' });
       // 'standard in/out inherit' says that whenever we create this child process, whatever logs, errors, or whatever else get emitted, take that information and pass it through to our programs console log
       // If we remove <stdio: 'inherit'> option we can not see any logs on terminal because we are no longer forwarding that output information back over to our terminal.
     }, 100);
